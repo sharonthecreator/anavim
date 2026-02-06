b9738ee4c48c83bfb8191f6b54ff5f36875a81f4
Sharon Naim <sharonthecreator@gmail.com>
2026-02-06 13:21:34 +0200

    fixed the new usage of lspconfig, and error from coq from client.tmux.enabled

--- lua/plugin-config/coq.lua
+++ lua/plugin-config/coq.lua
@@ -2,11 +2,13 @@ vim.g.coq_settings = {
     ["keymap.pre_select"] = true,
     ["completion.skip_after"] = {"{", "}", "[", "]"},
     ["display.ghost_text.enabled"] = false,
+    ["clients.tmux.enabled"] = false,
 }
 
-local lsp = require "lspconfig"
-local coq = require "coq"
+local coq = require("coq")
 
-lsp.ts_ls.setup{}
-lsp.ts_ls.setup(coq.lsp_ensure_capabilities{})
+local clangd_opts = coq.lsp_ensure_capabilities({})
+vim.lsp.config('clangd', clangd_opts)
+vim.lsp.enable('clangd')
+
 vim.cmd('COQnow -s')
--- lua/plugin-config/mason.lua
+++ lua/plugin-config/mason.lua
@@ -1,31 +1,6 @@
 require("mason").setup()
-local mason_lspconfig = require("mason-lspconfig")
-local lspconfig = require("lspconfig")
 
-lspconfig.pylsp.setup{
-  settings = {
-    pylsp = {
-      plugins = {
-        pycodestyle = {
-          ignore = {'W391', 'E501'},
-          maxLineLength = 100
-        }
-      }
-    }
-  }
-}
-
-
-mason_lspconfig.setup {
-    ensure_installed = {'bashls', 'lua_ls', 'pylsp', 'jsonls', 'clangd', 'cmake', 'rust_analyzer', 'texlab', 'ts_ls' },
+require("mason-lspconfig").setup({
+    ensure_installed = { 'clangd', 'pylsp', 'lua_ls', 'cmake' },
     automatic_installation = true,
-}
-
-mason_lspconfig.setup_handlers {
-  -- This is a default haudler that will be called for each installed server (also for new servers that are installed during a session)
-  function (server_name)
-    lspconfig[server_name].setup({
-      on_attach = on_attach,
-    })
-  end,
-}
+})
